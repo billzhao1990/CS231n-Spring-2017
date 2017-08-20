a9620aa3b9631395f9ace6bb32d511a30e9bbe35
Bill Zhao <billzhao1990@gmail.com>
2017-08-20 10:13:13 +0000

    modified SVM; add 2-layer NN; add feature extraction

--- assignment1/cs231n/classifiers/neural_net.py
+++ assignment1/cs231n/classifiers/neural_net.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
 from past.builtins import xrange
+import pdb
 
 class TwoLayerNet(object):
   """
@@ -40,8 +41,17 @@ class TwoLayerNet(object):
     self.params['b1'] = np.zeros(hidden_size)
     self.params['W2'] = std * np.random.randn(hidden_size, output_size)
     self.params['b2'] = np.zeros(output_size)
-
-  def loss(self, X, y=None, reg=0.0):
+    
+    self.params['W1_m'] = np.zeros((input_size, hidden_size))
+    self.params['W1_v'] = np.zeros((input_size, hidden_size))
+    self.params['W2_m'] = np.zeros((hidden_size, output_size))
+    self.params['W2_v'] = np.zeros((hidden_size, output_size))
+    self.params['b1_m'] = np.zeros(hidden_size)
+    self.params['b1_v'] = np.zeros(hidden_size)    
+    self.params['b2_m'] = np.zeros(output_size)
+    self.params['b2_v'] = np.zeros(output_size)
+    
+  def loss(self, X, y=None, reg=0.0, dropout_ratio=0.):
     """
     Compute the loss and gradients for a two layer fully connected neural
     network.
@@ -77,6 +87,18 @@ class TwoLayerNet(object):
     # shape (N, C).                                                             #
     #############################################################################
     pass
+    
+    ReLU = lambda x: np.maximum(0,x) # ReLU activation function
+    a_2 = X.dot(W1)+b1
+    h_2 = ReLU(a_2)
+    
+    #--------implement dropout
+    if dropout_ratio != 0.:
+        mask = (np.random.rand(*h_2.shape) < dropout_ratio)/dropout_ratio # indicate where to dropout
+        h_2 *= mask # dropout!
+    #--------dropout finish
+    
+    scores = h_2.dot(W2)+b2
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
@@ -94,6 +116,9 @@ class TwoLayerNet(object):
     # classifier loss.                                                          #
     #############################################################################
     pass
+    scores_exp = np.exp(scores)
+    softmax = scores_exp[range(N), y]/np.sum(scores_exp,axis=1)
+    loss = 1 / float(N) * np.sum(-np.log(softmax)) + reg*(np.sum(W1*W1)+np.sum(W2*W2))
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
@@ -105,7 +130,48 @@ class TwoLayerNet(object):
     # and biases. Store the results in the grads dictionary. For example,       #
     # grads['W1'] should store the gradient on W1, and be a matrix of same size #
     #############################################################################
-    pass
+    
+    #------------------------------- The verbose form ---------------------------
+    d_loss = 1.
+    d_aver = 1 / float(N) * d_loss  
+    d_minus_1 = -1 * d_aver
+    d_log = 1./softmax * d_minus_1
+    
+    # for classes that k != y 
+    d_scores = -scores_exp / np.sum(scores_exp, axis=1, keepdims=True) * softmax[:,None] * d_log[:,None]
+    # for classes that k=y
+    d_scores[range(N), y] += softmax * d_log
+    dW2 = h_2.T.dot(d_scores)
+    dh_2 = d_scores.dot(W2.T)
+    db2 = np.sum(d_scores, axis=0)
+    da_2 = np.maximum(0, a_2)/a_2 * dh_2
+    dW1 = X.T.dot(da_2)
+    db1 = np.sum(da_2, axis=0)
+    #--------------------------------------------------------------------
+    
+    #============================== The compact form ============================== 
+    #d_scores = scores_exp/np.sum(scores_exp,axis=1, keepdims=True)
+    #d_scores[range(N), y] -= 1.
+    #d_scores /= float(N)
+    # backpropate the gradient to the parameters
+    # first backprop into parameters W2 and b2
+    #dW2 = np.dot(h_2.T, d_scores)
+    #db2 = np.sum(d_scores, axis=0, keepdims=True)
+    # next backprop into hidden layer
+    #dhidden = np.dot(d_scores, W2.T)
+    # backprop the ReLU non-linearity
+    #dhidden[h_2 <= 0] = 0
+    # finally into W,b
+    #dW1 = np.dot(X.T, dhidden)
+    #db1 = np.sum(dhidden, axis=0, keepdims=True)
+    #============================================================================== 
+    
+    dW2 += 2 * reg * W2
+    dW1 += 2 * reg * W1
+    grads['W2'] = dW2
+    grads['W1'] = dW1
+    grads['b2'] = db2
+    grads['b1'] = db1
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
@@ -115,7 +181,8 @@ class TwoLayerNet(object):
   def train(self, X, y, X_val, y_val,
             learning_rate=1e-3, learning_rate_decay=0.95,
             reg=5e-6, num_iters=100,
-            batch_size=200, verbose=False):
+            batch_size=200, verbose=False,
+           d_r=0., opt_alg='SGD'): # append the dropout ratio
     """
     Train this neural network using stochastic gradient descent.
 
@@ -141,6 +208,16 @@ class TwoLayerNet(object):
     train_acc_history = []
     val_acc_history = []
 
+    # parameters for adam algorithm
+    W1_m = self.params['W1_m']
+    W1_v = self.params['W1_v']
+    W2_m = self.params['W2_m']
+    W2_v = self.params['W2_v']
+    b1_m = self.params['b1_m']
+    b1_v = self.params['b1_v']   
+    b2_m = self.params['b2_m']
+    b2_v = self.params['b2_v']
+    
     for it in xrange(num_iters):
       X_batch = None
       y_batch = None
@@ -150,12 +227,15 @@ class TwoLayerNet(object):
       # them in X_batch and y_batch respectively.                             #
       #########################################################################
       pass
+      indices_batch = np.random.choice(num_train, batch_size, replace = True)
+      X_batch = X[indices_batch]
+      y_batch = y[indices_batch]
       #########################################################################
       #                             END OF YOUR CODE                          #
       #########################################################################
 
       # Compute loss and gradients using the current minibatch
-      loss, grads = self.loss(X_batch, y=y_batch, reg=reg)
+      loss, grads = self.loss(X_batch, y=y_batch, reg=reg, dropout_ratio=d_r)
       loss_history.append(loss)
 
       #########################################################################
@@ -165,6 +245,39 @@ class TwoLayerNet(object):
       # stored in the grads dictionary defined above.                         #
       #########################################################################
       pass
+    
+      if opt_alg == 'SGD':
+        self.params['W2'] -= learning_rate*grads['W2']
+        self.params['W1'] -= learning_rate*grads['W1']
+        self.params['b2'] -= learning_rate*grads['b2']
+        self.params['b1'] -= learning_rate*grads['b1']
+      if opt_alg == 'Adam':
+        t=it+1
+        
+        W2_m = 0.9*W2_m + 0.1*grads['W2']
+        W2_mt = W2_m / (1-0.9**t)
+        W2_v = 0.999*W2_v + 0.001*(grads['W2']**2)
+        W2_vt = W2_v / (1-0.999**t)
+        self.params['W2'] += - learning_rate * W2_mt / (np.sqrt(W2_vt) + 1e-8)
+        
+        b2_m = 0.9*b2_m + 0.1*grads['b2']
+        b2_mt = b2_m / (1-0.9**t)
+        b2_v = 0.999*b2_v + 0.001*(grads['b2']**2)
+        b2_vt = b2_v / (1-0.999**t)
+        self.params['b2'] += - learning_rate * b2_mt / (np.sqrt(b2_vt) + 1e-8)
+
+        W1_m = 0.9*W1_m + 0.1*grads['W1']
+        W1_mt = W1_m / (1-0.9**t)
+        W1_v = 0.999*W1_v + 0.001*(grads['W1']**2)
+        W1_vt = W1_v / (1-0.999**t)
+        self.params['W1'] += - learning_rate * W1_mt / (np.sqrt(W1_vt) + 1e-8)
+        
+        b1_m = 0.9*b1_m + 0.1*grads['b1']
+        b1_mt = b1_m / (1-0.9**t)
+        b1_v = 0.999*b1_v + 0.001*(grads['b1']**2)
+        b1_vt = b1_v / (1-0.999**t)
+        self.params['b1'] += - learning_rate * b1_mt / (np.sqrt(b1_vt) + 1e-8)
+
       #########################################################################
       #                             END OF YOUR CODE                          #
       #########################################################################
@@ -210,6 +323,11 @@ class TwoLayerNet(object):
     # TODO: Implement this function; it should be VERY simple!                #
     ###########################################################################
     pass
+    a2 = X.dot(self.params['W1']) + self.params['b1']
+    ReLU = lambda x: np.maximum(0,x) # ReLU activation function
+    h2 =ReLU(a2)
+    scores_matrix = h2.dot(self.params['W2']) + self.params['b2']
+    y_pred = np.argmax(scores_matrix, axis=1)
     ###########################################################################
     #                              END OF YOUR CODE                           #
     ###########################################################################
